41c5b4a4b1e829ec961ec79ebfbb3c912c8fdbe1
Margaret T Harmon <77816163+MargaretHarmon@users.noreply.github.com>
2021-03-22 18:54:45 -0600

    Add files via upload

---
+++ homework 9 p5.js/sketch.js
@@ -0,0 +1,22 @@
+function setup() {
+  createCanvas(400, 500);
+}
+
+function draw() {
+  background(123,80,129);
+  circle(128,180,50);
+  circle(260,180,50);
+  ellipse(200,300,300,50);
+  square(128,180,20);
+  square(260,180,20);
+  rect(165,45,60,80);
+  ellipse(195,125,100,20);
+  triangle(30,75,58,20,86,75);
+  point(100,75);
+  line(50,300, 350, 300);
+  textSize(20);
+  text('Margaret Harmon', 220,480);
+  text('frog lady', 160,30);
+  
+  
+}
